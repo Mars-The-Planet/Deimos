b7ab67a236707d211ed6b8b9674ebd4ef24470e5
Mars <ruzicka.maty@gmail.com>
2025-11-15 01:15:09 +0100

    improved shaped recipe generation

--- common/src/main/java/com/mars/deimos/CommonClass.java
+++ common/src/main/java/com/mars/deimos/CommonClass.java
@@ -1,7 +1,16 @@
 package com.mars.deimos;
 
+import com.google.common.collect.Lists;
+import com.mars.deimos.datagen.DeimosRecipeGenerator;
+
 public class CommonClass {
     public static void init() {
-
+        DeimosRecipeGenerator.createShapedRecipeJson(
+                Lists.newArrayList("stick", "minecraft:stone"),
+                Lists.newArrayList(
+                        "#B",
+                        "##"
+                ),
+                "stone");
     }
 }
