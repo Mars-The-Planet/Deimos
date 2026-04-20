33ef3405defd4476be3123fe10885daa9711c232
xruzicm00 <xruzicm00@stud.fit.vut.cz>
2026-04-20 15:11:18 +0200

    fixed services safety

--- common/src/main/java/com/mars/deimos/platform/Services.java
+++ common/src/main/java/com/mars/deimos/platform/Services.java
@@ -7,6 +7,6 @@ import java.util.ServiceLoader;
 public class Services {
     public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
     public static <T> T load(Class<T> clazz) {
-        return ServiceLoader.load(clazz).findFirst().orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+        return ServiceLoader.load(clazz, Services.class.getClassLoader()).findFirst().orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
     }
 }
