c52ef6519ce44cdadd31082ee8110d3d8484ed4b
Mars-The-Planet <microsoft.maty@gmail.com>
2026-06-12 18:20:14 +0200

    added mod id to recipes

--- common/src/main/java/com/mars/deimos/mixin/RecipeManagerMixin.java
+++ common/src/main/java/com/mars/deimos/mixin/RecipeManagerMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
+import static com.mars.deimos.Constants.MOD_ID;
 import static com.mars.deimos.datagen.DeimosRecipeGenerator.RECIPES;
 
 @Mixin(value = RecipeManager.class, priority = 1100)
@@ -22,7 +23,7 @@ public class RecipeManagerMixin {
             int i = 0;
             for(JsonElement jsonElement : RECIPES){
                 if(jsonElement != null){
-                    map.put(new ResourceLocation("deimosgeneratedcrafting" + i), jsonElement);
+                    map.put(new ResourceLocation(MOD_ID, "deimosgeneratedcrafting" + i), jsonElement);
                     i++;
                 }
             }
